8fb538c5ec4fb8223ae744fe03c3162ca5d2f47b
Christiana Ukoli <cukoli22@gmail.com>
2022-06-05 15:01:56 -0500

    working state capitals api

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const app = express()
+const PORT = 8000
+
+const states = {
+    "tennessee": {
+        "capital": "Nashville",
+        "abbreviation": "TN",
+        "cap population": 669000
+    },
+    "texas": {
+        "capital": "Austin",
+        "abbreviation": "TX",
+        "cap population": 954000
+    },
+    "unknown":{
+        "capital": "unknown",
+        "abbreviation": "unknown",
+        "cap population": 0
+    }
+}
+app.get("/", (request, response)=>{
+    response.sendFile(__dirname + "/index.html")
+})
+
+app.get("/api/:name", (request, response)=>{
+    const stateName = request.params.name.toLowerCase()
+    
+    if(states[stateName]){
+        response.json(states[stateName])
+    }else{
+        response.json(states["unknown"])
+    }
+    
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is now running on port ${PORT}! Better go catch it!`)
+})
