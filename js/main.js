5c4f0cc9cb7c58192232b9704a92a9463eba92d3
Danita Codes <danitacodes@gmail.com>
2022-06-29 20:05:45 -0700

    changes

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//State Dropdown
+let dropdown = document.getElementById('states');
+dropdown.length = 0;
+
+let defaultOption = document.createElement('option');
+defaultOption.text = 'Choose a State';
+
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
+
+const url = 'https://state-capitals-api.herokuapp.com/api/state-capitals';
+
+fetch(url)
+.then (
+    function(res) {
+        if (res.status !== 200) {
+            console.warn('Error')
+            return;
+        }
+        res.json().then(function(data) {
+           
+           let option;
+
+            for (let i = 0; i < data.length; i++) {
+                option = document.createElement('option');
+                option.text = data[i].capital;
+                dropdown.add(option)
+            }
+        });
+    }
+)
+.catch(function(err) {
+    console.error(`There is an error: ${err}`)
+})
